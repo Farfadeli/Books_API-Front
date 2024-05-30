aded1037b8bb755e4140b3760369695ccc6e6ac4
Nicolas Daunac <lust@MacBook-Air-de-Nicolas.local>
2024-05-30 12:55:27 +0200

    init

---
+++ src/components/MainView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import '../styles/style.css'
+
+const MainView = () => {
+    return(
+        <div className="container">
+            <Header />
+        </div>
+    )
+}
+
+export default MainView
